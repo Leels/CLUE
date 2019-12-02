6920784f7cec569aa6cd0fb0dfc7420510b66422
jonathansalfen <jonathan.salfen@gmail.com>
2019-12-02 11:38:34 -0800

    add logic to game object

--- src/game.js
+++ src/game.js
@@ -1,6 +1,16 @@
 export class Game {
   constructor() {
-    
+
+    this.rooms = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ];
+    this.weapons = [];
+    this.suspects = [];
+    this.state = active;
+    this.caseFile = [];
+    this.turnNumber = 0;
   }
 
 
