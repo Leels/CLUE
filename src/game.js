1b005d1b2fe229d3471da4b831c0510a03d1f6c6
Student <example@example.com>
2019-12-02 15:15:55 -0800

    Add clue distribution to suspects

--- src/game.js
+++ src/game.js
@@ -12,6 +12,7 @@ export class Game {
         this.weapons = ['Revolver', 'Rope', 'Lead Pipe', 'Knife', 'Candlestick', 'Wrench'];
         this.suspects = [];
         this.caseFile = [];
+        this.clues = ['Study', 'Library', 'Lounge', 'Game Room', 'Hall', 'Dining Room', 'Conservatory', 'Ballroom', 'Kitchen', 'Revolver', 'Rope', 'Lead Pipe', 'Knife', 'Candlestick', 'Wrench', 'white', 'blue', 'green', 'purple', 'yellow', 'red'];
         this.turnNumber = 0;
 
         this.createMystery();
@@ -21,12 +22,19 @@ export class Game {
     initSuspects(playerColor) {
         let colors = ['white', 'blue', 'green', 'purple', 'yellow', 'red'];
         let locations = this.rooms.map((x) => x);
-        for (let i = 0; i <=6; i++) {
+        for (let i = 0; i < 6; i++) {
             const randomColor = getRandomFrom(colors);
             const randomLoc = getRandomFrom(locations);
+            let randomClues = [];
+
+            for (let j = 0; j < 3; j++) {
+                const clue = getRandomFrom(this.clues);
+                this.clues.splice(clue.index, 1);
+                randomClues.push(clue.value);
+            }
 
             this.suspects.push(
-                new Suspect(randomColor.value, [], (randomColor.value === playerColor), randomLoc.value)
+                new Suspect(randomColor.value, randomClues, (randomColor.value === playerColor), randomLoc.value)
             );
 
             colors.splice(randomColor.index, 1);
@@ -36,8 +44,17 @@ export class Game {
 
     createMystery() {
         let colors = ['white', 'blue', 'green', 'purple', 'yellow', 'red'];
-        this.caseFile.push(getRandomFrom(colors).value);
-        this.caseFile.push(getRandomFrom(this.weapons).value);
-        this.caseFile.push(getRandomFrom(this.rooms).value);
+
+        const murderer = getRandomFrom(colors);
+        this.caseFile.push(murderer.value);
+        this.clues.splice(this.clues.indexOf(murderer.value), 1);
+
+        const murderWeapon = getRandomFrom(this.weapons);
+        this.caseFile.push(murderWeapon.value);
+        this.clues.splice(this.clues.indexOf(murderWeapon.value), 1);
+
+        const murderLocation = getRandomFrom(this.rooms);
+        this.caseFile.push(murderLocation.value);
+        this.clues.splice(this.clues.indexOf(murderLocation.value), 1);
     }
 }
