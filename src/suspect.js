7bcc2daffddb19cc7081f00da33e32f703369468
Student <example@example.com>
2019-12-05 13:41:25 -0800

    Minor fixes

--- src/suspect.js
+++ src/suspect.js
@@ -2,7 +2,7 @@ export const gameVars = {
     guests: ['Mrs. White', 'Mrs. Peacock', 'Mr. Green', 'Prof. Plum', 'Col. Mustard', 'Ms. Scarlet'],
     rooms: ['Study', 'Library', 'Lounge', 'Billiard Room', 'Hall', 'Dining Room', 'Conservatory', 'Ballroom', 'Kitchen'],
     weapons: ['Revolver', 'Rope', 'Lead Pipe', 'Knife', 'Candlestick', 'Wrench']
-}
+};
 
 export class Suspect {
     constructor(name, knowledge, isHuman, isMurderer) {
